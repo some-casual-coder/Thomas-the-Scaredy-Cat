5265356c3411d9e0004f753d7aa2d31a288c2286
nalugala-vc <vanessachebukwa@gmail.com>
2023-11-29 11:35:09 +0300

    Formated the timer text

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -12,6 +12,8 @@ public class Timer : MonoBehaviour
     void Update()
     {
         elapsedTime += Time.deltaTime;
-        timerText.text = elapsedTime.ToString();
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}",minutes,seconds);
     }
 }
